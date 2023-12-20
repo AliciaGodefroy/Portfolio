7ca9cf7ad6eb1d21c9d8f7bd32ec725b94e22cec
Alicia Godefroy <alicia.godefroy@fk-agency.com>
2023-12-20 22:37:54 +0100

    first commit

---
+++ front-page.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+
+<div class="main">
+    <section class="main_left">
+        <div class="main_left-ctn">
+            <div class="main_left-ttl">
+                <h1>Alicia Godefroy</h1>
+            </div>
+            <div class="main_left-desc">
+                <p>Développeuse Front-End.
+                Actuellement en poste au sein de l’agence FK Agency.</p>
+            </div>
+        </div>
+    </section>
+    <section class="main_right">
+        <nav id="site-navigation" class="main-navigation" role="navigation">
+            <?php
+            wp_nav_menu( array(
+                'theme_location' => 'header-menu',
+                'menu_class'     => 'menu',
+            ) );
+            ?>
+        </nav>
+    </section>
+</div>
+
+<?php get_footer(); ?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+
+<div class="hero">
+    <h1 class="hero_ttl">Portfolio</h1>
+</div>
+
+<?php get_footer(); ?>
